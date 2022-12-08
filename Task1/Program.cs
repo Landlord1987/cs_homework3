ce463667422612c01d95dd57d00a9b5d8af75b18
Denis Krinitsyn <skylinekrin@gmail.com>
2022-12-08 22:07:47 +0500

    Task1 upgrade

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,18 +1,25 @@
 ﻿Boolean flag = true;
 int num = 0;
 
-//Проверка на количество введенных цифр
-while (flag == true)
-{
-    Console.WriteLine("Введите 5 цифр: ");
-    num = Convert.ToInt32(Console.ReadLine());
-    
-    if(num < 100000 && num >= 10000) flag = !flag;
-    else Console.WriteLine("Вы ввели не 5 цифр, пробуйте снова!");
-}
-
+//Вызов метода ввода 
+vvod();
 // Вызов checkPalindrom
-checkPalindrom(num); 
+checkPalindrom(num);
+
+//Метод проверки на количество введенных цифр
+int vvod()
+{
+
+    while (flag == true)
+    {
+        Console.WriteLine("Введите 5 цифр: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        if (num < 100000 && num >= 10000) flag = !flag;
+        else Console.WriteLine("Вы ввели не 5 цифр, пробуйте снова!");
+    }
+    return num;
+}
 
 //Метод проверки палиндрома
 void checkPalindrom(int num)
@@ -23,7 +30,7 @@ void checkPalindrom(int num)
     for (int i = masNum.Length - 1; i >= 0; i--)
     {
         masNum[i] = g % 10;
-        g = g / 10;        
+        g = g / 10;
     }
 
     for (int i = 0, j = masNum.Length - 1; i <= masNum.Length / 2; i++, j--)
