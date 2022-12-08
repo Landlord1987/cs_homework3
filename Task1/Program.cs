c695829b586799db45241514ba31ad6dbdebf67d
Denis Krinitsyn <skylinekrin@gmail.com>
2022-12-08 22:00:49 +0500

    Task1 done

---
+++ Task1/Program.cs
@@ -0,0 +1,41 @@
+﻿Boolean flag = true;
+int num = 0;
+
+//Проверка на количество введенных цифр
+while (flag == true)
+{
+    Console.WriteLine("Введите 5 цифр: ");
+    num = Convert.ToInt32(Console.ReadLine());
+    
+    if(num < 100000 && num >= 10000) flag = !flag;
+    else Console.WriteLine("Вы ввели не 5 цифр, пробуйте снова!");
+}
+
+// Вызов checkPalindrom
+checkPalindrom(num); 
+
+//Метод проверки палиндрома
+void checkPalindrom(int num)
+{
+    int[] masNum = new int[5];
+    int g = num;
+
+    for (int i = masNum.Length - 1; i >= 0; i--)
+    {
+        masNum[i] = g % 10;
+        g = g / 10;        
+    }
+
+    for (int i = 0, j = masNum.Length - 1; i <= masNum.Length / 2; i++, j--)
+    {
+        if (masNum[i] != masNum[j])
+        {
+            Console.WriteLine("Это не палиндром");
+            break;
+        }
+        else if (i == masNum.Length / 2)
+        {
+            Console.WriteLine("Это палиндром");
+        }
+    }
+}
